6b5046d2a215066770b1edea77d438ec56c6baad
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-05-23 22:52:25 -0400

    initial test

---
+++ WebAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace WebAPI.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
